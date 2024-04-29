c5a90d6a20f1fd1d450b8dd356f4416bce34f396
Mezdelex <alexcondegomez@gmail.com>
2024-04-29 21:25:58 +0200

    chore(dependencies): updated dependencies

--- src/services/projects/projectsService.ts
+++ src/services/projects/projectsService.ts
@@ -5,14 +5,14 @@ import IProjectsService from "./IProjectsService";
 import _ from "lodash";
 
 export const projectsService: IProjectsService = {
-    sortProjects: () => {
-        if (repositoriesStore.repos.length)
-            projectsStore.projects = _.chain(projectsData)
-                .map(project => project.id !== ""
-                    ? { ...project, pushed_at: repositoriesStore.repos.find(repo => repo.name === project.id)!.pushed_at }
-                    : project)
-                .orderBy(project => project.pushed_at, 'desc').value();
-        else
-            projectsStore.projects = projectsData;
-    }
+  sortProjects: () => {
+    if (repositoriesStore.repos.length)
+      projectsStore.projects = _.chain(projectsData)
+        .map(project => project.id !== ""
+          ? { ...project, pushed_at: repositoriesStore.repos.find(repo => repo.name === project.id)!.pushed_at }
+          : project)
+        .orderBy(project => project.pushed_at, 'desc').value();
+    else
+      projectsStore.projects = projectsData;
+  }
 };
